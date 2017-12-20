003b3939bc4fafdd110a5634b200821f0210ea36
jimgong <jimgong3@gmail.com>
2017-12-20 18:19:56 +0800

    upload from web page

--- isermon-server/index.js
+++ isermon-server/index.js
@@ -7,6 +7,9 @@ var logger = new (winston.Logger)({
     new (winston.transports.File)({ filename: './logs/index.log' })
   ]
 });
+var formidable = require('formidable');
+var fs = require('fs');
+
 var uploadUtil = require('./uploadUtil');
 var sermonsUtil = require('./sermonsUtil');
 
@@ -51,3 +54,36 @@ app.get('/sermons', function (req, res) {
     logger.info("index>> GET /sermons complete");
   })
 })
+
+app.get('/upload', function (req, res) {
+  logger.info("index>> GET /upload");
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+  res.write('<input type="text" name="title" value="title"><br><br>');
+  res.write('<input type="text" name="description" value="write something about it..."><br><br>');
+  res.write('<input type="file" name="filetoupload"><br><br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  return res.end();
+})
+
+app.post('/fileupload', function (req, res) {
+  logger.info("index>> POST /fileupload");
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+	  var title = fields.title;
+	  logger.info("title: " + title);
+	  var description = fields.description;
+	  logger.info("description: " + description);
+      var oldpath = files.filetoupload.path;
+	  logger.info("oldpath: " + oldpath);
+      var newpath = 'upload/' + files.filetoupload.name;
+	  logger.info("newpath: " + newpath);
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+	  });
+  });
+})
+
