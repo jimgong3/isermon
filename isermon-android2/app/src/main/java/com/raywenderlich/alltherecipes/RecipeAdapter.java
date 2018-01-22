e39f216d6d25386c37d37a2e073226b5c2202444
jimgong <jimgong3@gmail.com>
2018-01-22 21:55:13 +0800

    -

---
+++ isermon-android2/app/src/main/java/com/raywenderlich/alltherecipes/RecipeAdapter.java
@@ -0,0 +1,109 @@
+package com.raywenderlich.alltherecipes;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by jim on 22/1/2018.
+ */
+
+public class RecipeAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private ArrayList<Recipe> mDataSource;
+
+    private static final HashMap<String, Integer> LABEL_COLORS = new HashMap<String, Integer>() {{
+        put("Low-Carb", R.color.colorLowCarb);
+        put("Low-Fat", R.color.colorLowFat);
+        put("Low-Sodium", R.color.colorLowSodium);
+        put("Medium-Carb", R.color.colorMediumCarb);
+        put("Vegetarian", R.color.colorVegetarian);
+        put("Balanced", R.color.colorBalanced);
+    }};
+
+    public RecipeAdapter(Context context, ArrayList<Recipe> items) {
+        mContext = context;
+        mDataSource = items;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    //1
+    @Override
+    public int getCount() {
+        return mDataSource.size();
+    }
+
+    //2
+    @Override
+    public Object getItem(int position) {
+        return mDataSource.get(position);
+    }
+
+    //3
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    //4
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get view for row item
+        View rowView = mInflater.inflate(R.layout.list_item_recipe, parent, false);
+
+        // Get title element
+        TextView titleTextView =
+                (TextView) rowView.findViewById(com.raywenderlich.alltherecipes.R.id.recipe_list_title);
+
+// Get subtitle element
+        TextView subtitleTextView =
+                (TextView) rowView.findViewById(com.raywenderlich.alltherecipes.R.id.recipe_list_subtitle);
+
+// Get detail element
+        TextView detailTextView =
+                (TextView) rowView.findViewById(com.raywenderlich.alltherecipes.R.id.recipe_list_detail);
+
+// Get thumbnail element
+        ImageView thumbnailImageView =
+                (ImageView) rowView.findViewById(com.raywenderlich.alltherecipes.R.id.recipe_list_thumbnail);
+
+        // 1
+        Recipe recipe = (Recipe) getItem(position);
+
+// 2
+        titleTextView.setText(recipe.title);
+        subtitleTextView.setText(recipe.description);
+        detailTextView.setText(recipe.label);
+
+// 3
+        Picasso.with(mContext).load(recipe.imageUrl).placeholder(R.mipmap.ic_launcher).into(thumbnailImageView);
+
+        Typeface titleTypeFace = Typeface.createFromAsset(mContext.getAssets(), "fonts/JosefinSans-Bold.ttf");
+        titleTextView.setTypeface(titleTypeFace);
+
+        Typeface subtitleTypeFace =
+                Typeface.createFromAsset(mContext.getAssets(), "fonts/JosefinSans-SemiBoldItalic.ttf");
+        subtitleTextView.setTypeface(subtitleTypeFace);
+
+        Typeface detailTypeFace = Typeface.createFromAsset(mContext.getAssets(), "fonts/Quicksand-Bold.otf");
+        detailTextView.setTypeface(detailTypeFace);
+
+        detailTextView.setTextColor(ContextCompat.getColor(mContext, LABEL_COLORS.get(recipe.label)));
+
+        return rowView;
+    }
+
+}
